4bef9f1e9d7d7d344647a57fa4e9fba194ef5686
kshitijbarya0 <kshitijbarya123@gmail.com>
2025-01-13 01:49:50 +0530

    some small changes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 const app = express()
-app.use(cors());
+app.use(cors()); // cross origin resource sharing means in simple if i want to allow my friend to enter in my home then its is legal but what if someone who is unkown to me if he/she enter in my home that illegal and the same work CORS do here its allow only know Origin request
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded());
+app.use(express.urlencoded());// example like you have seen in URL Kshtij+barya=? or kshitij%20barya for handle this kind of content we will use cookieParser
 app.use(express.static("public"));
-app.use(cookieParser());
+app.use(cookieParser());  
+
 
 export default app
