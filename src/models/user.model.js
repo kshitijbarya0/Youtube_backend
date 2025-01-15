3cb017947df30cd939b1f1d7c498485cd4835e80
kshitijbarya0 <kshitijbarya123@gmail.com>
2025-01-16 00:21:47 +0530

    added JWT,TOKENs, bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userModel = new mongoose.Schema({
     username:{
         type:String,
@@ -47,5 +48,38 @@ const userModel = new mongoose.Schema({
     ]
 
 },{timestamps:true})
+
+userModel.pre("save",async function (next){
+    if(!this.isModified("password")){
+      return next();
+    }
+    this.password = bcrypt.hash(this.password,10)
+    next();
+})
+userModel.method.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+userModel.method.generateAccessToken = function(){
+     return jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+         expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      }
+    )
+}
+userModel.method.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+          _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+           expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+      )
+}
 
 export const User = mongoose.model("User",userModel);
